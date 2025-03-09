c3aa63bac0038d45d7fe918f88fae0cdbc57f9ae
Pastilhamas <74551913+Pastilhamas@users.noreply.github.com>
2025-03-09 14:52:30 +0000

    login/logout/profile working

--- app/src/index.js
+++ app/src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Auth0Provider} from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      cacheLocation="localstorage" 
+      useRefreshTokens={true}
     >
       <App />
     </Auth0Provider>
   </React.StrictMode>
 );
-
