ffbf3c7d4a5713fd1bf5f9fd9bfa7a39f93eea55
Pastilhamas <74551913+Pastilhamas@users.noreply.github.com>
2025-03-09 14:43:59 +0000

    update profile page

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,23 @@
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import {useAuth0} from "@auth0/auth0-react";
 import './App.css';
 
 function App() {
+  const {isLoading, error} = useAuth0();
   return (
     <main>
       <h1>Auth0 Login</h1>
-      <LoginButton />
-      <LogoutButton />
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading &&(
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+          </>
+      )}
     </main>
   );
 }
