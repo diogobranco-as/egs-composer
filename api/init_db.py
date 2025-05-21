bc0df296e5ab00093adbee0b74fcdb08877b3bce
Pastilhamas <74551913+Pastilhamas@users.noreply.github.com>
2025-05-21 13:39:17 +0100

    container working

--- api/init_db.py
+++
@@ -1,7 +0,0 @@
-# api/init_db.py
-from database import Base, engine
-import models  
-
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    print("✅ Tables created or already exist.")
